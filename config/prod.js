483484d2b0e8d400e589bf74e99cc27ec5f9dbd3
Dvir Cohen <dvirco123@gmail.com>
2022-11-27 11:17:10 +0200

    second

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,3 @@
 module.exports = {
-    // dbURL: 'mongodb+srv://caseppp:1234@cluster0.948vmui.mongodb.net/?retryWrites=true&w=majority',
-    dbURL: 'mongodb+srv://dvirco123:1234@cluster0.gusfzov.mongodb.net/?retryWrites=true&w=majority',
+    dbURL: 'mongodb+srv://<username>:<password>@cluster0.948vmui.mongodb.net/?retryWrites=true&w=majority',
 }
